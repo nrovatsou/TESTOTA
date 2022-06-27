5036b31618f2bdbc9180829e02a69b49108c802d
nrovatsou <nrovatsou@nydorsystem.com>
2022-06-27 14:48:01 +0300

    R2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from time import sleep
 
 
 def download_and_install_update_if_available():
-     o = OTAUpdater('https://github.com/nrovatsou/TESTOTA.git')
+     o = OTAUpdater('https://github.com/nrovatsou/TESTOTA')
      o.install_update_if_available_after_boot('COSMOTE-179663', 'DYR7K3QY7HDU469A')
 
 def start():
